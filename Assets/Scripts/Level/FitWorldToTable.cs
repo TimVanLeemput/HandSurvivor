61c610ed6dfc6c990978316afe2fd75d51c3cab3
Tim VL <vanleemput.tim@gmail.com>
2025-11-14 12:22:22 +0100

    [MainMenu scene created]

--- Assets/Scripts/Level/FitWorldToTable.cs
+++ Assets/Scripts/Level/FitWorldToTable.cs
@@ -5,9 +5,6 @@ using HandSurvivor;
 
 public class FitWorldToTable : MonoBehaviour
 {
-    [Header("NavMesh")] [SerializeField] private bool rebuildNavMesh = true;
-    [SerializeField] private float navMeshRebuildDelay = 0.1f;
-
     [Header("References")] [SerializeField]
     private SceneLoader sceneLoader;
 
@@ -63,61 +60,6 @@ public class FitWorldToTable : MonoBehaviour
         worldReference.transform.position = worldPlacementReference.transform.position;
         worldReference.transform.rotation = worldPlacementReference.transform.rotation;
         worldReference.transform.localScale = worldPlacementReference.transform.localScale / 100;
-
-        if (rebuildNavMesh)
-        {
-            DisableNavMeshAgents();
-            // StartCoroutine(RebuildNavMesh());
-            // StartCoroutine(EnableNavMesh());
-            // worldReference.NavMeshSurface.enabled = true;
-        }
-    }
-
-    private void DisableNavMeshAgents()
-    {
-        UnityEngine.AI.NavMeshAgent[] agents = worldReference.GetComponentsInChildren<UnityEngine.AI.NavMeshAgent>();
-        foreach (UnityEngine.AI.NavMeshAgent agent in agents)
-        {
-            agent.enabled = false;
-        }
-    }
-
-    private IEnumerator EnableNavMesh()
-    {
-        yield return new WaitForSeconds(navMeshRebuildDelay);
-        worldReference.NavMeshSurface.enabled = true;
-    }
-
-    private IEnumerator RebuildNavMesh()
-    {
-        yield return new WaitForSeconds(navMeshRebuildDelay);
-
-        NavMeshSurface[] surfaces = worldReference.GetComponentsInChildren<NavMeshSurface>();
-
-        if (surfaces.Length == 0)
-        {
-            yield break;
-        }
-
-        foreach (NavMeshSurface surface in surfaces)
-        {
-            if (surface == null) continue;
-
-            surface.RemoveData();
-            surface.BuildNavMesh();
-        }
-
-        Debug.Log($"[FitWorldToTable] NavMesh rebuilt");
-
-        yield return new WaitForEndOfFrame();
-
-        UnityEngine.AI.NavMeshAgent[] agents = worldReference.GetComponentsInChildren<UnityEngine.AI.NavMeshAgent>();
-        foreach (UnityEngine.AI.NavMeshAgent agent in agents)
-        {
-            agent.enabled = true;
-        }
-
-        Debug.Log($"[FitWorldToTable] NavMeshAgents re-enabled");
     }
 
     [ContextMenu("Refit World")]
--- Assets/Scripts/Level/WorldReference.cs
+++ Assets/Scripts/Level/WorldReference.cs
@@ -1,34 +1,6 @@
-using System;
-using Unity.AI.Navigation;
 using UnityEngine;
 
-/// <summary>
-/// Represents the root of a game world/level that needs to be positioned and scaled.
-/// Used by SceneLoader to fit the world onto the calibrated table surface.
-/// </summary>
 public class WorldReference : MonoBehaviour
 {
-    public Renderer WorldPlaneRenderer = null;
-    public NavMeshSurface NavMeshSurface = null;
-    public Vector3 TopLeftCorner = Vector3.zero;
-    public Vector3 TopRightCorner = Vector3.zero;
-    public Vector3 BottomLeftCorner = Vector3.zero;
-    public Vector3 BottomRightCorner = Vector3.zero;
 
-    private void Start()
-    {
-        GetWorldPlaneBounds();
-    }
-
-    public void GetWorldPlaneBounds()
-    {
-        if (WorldPlaneRenderer != null)
-        {
-            Bounds bounds = WorldPlaneRenderer.bounds;
-            TopLeftCorner = new Vector3(bounds.max.x, bounds.min.y, bounds.min.z); // Top-left
-            TopRightCorner = new Vector3(bounds.max.x, bounds.min.y, bounds.max.z); // Top-right
-            BottomLeftCorner = new Vector3(bounds.min.x, bounds.min.y, bounds.min.z); // Bottom-left
-            BottomRightCorner = new Vector3(bounds.min.x, bounds.min.y, bounds.max.z); // Bottom-right
-        }
-    }
 }
