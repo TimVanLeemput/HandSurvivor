c5b777158b20316c4674dc33d20f29c77e45a41d
Tim VL <vanleemput.tim@gmail.com>
2025-11-20 19:54:22 +0100

    [Adding wrist button + HandMenu]

---
+++ Assets/Scripts/WristMenuToggle.cs
@@ -0,0 +1,63 @@
+using Oculus.Interaction;
+using Oculus.Interaction.Locomotion;
+using UnityEngine;
+
+public class WristMenuToggle : MonoBehaviour
+{
+    [Header("Menu")]
+    [SerializeField]
+    private GameObject _menu;
+
+    [Header("Wrist Angle Active State")]
+    [SerializeField]
+    private WristAngleActiveState _wristAngleActiveState;
+
+    [Header("Debug")]
+    [SerializeField]
+    private bool _showDebugLogs = false;
+
+    private bool _wasActive = false;
+
+    private void Start()
+    {
+        if (_wristAngleActiveState == null)
+        {
+            Debug.LogError($"WristAngleActiveState not assigned on {gameObject.name}");
+            enabled = false;
+            return;
+        }
+
+        if (_menu == null)
+        {
+            Debug.LogError($"Menu GameObject not assigned on {gameObject.name}");
+            enabled = false;
+            return;
+        }
+
+        _menu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        bool isActive = _wristAngleActiveState.Active;
+
+        if (isActive && !_wasActive)
+        {
+            _menu.SetActive(true);
+            if (_showDebugLogs)
+            {
+                Debug.Log("<color=green>Wrist menu ACTIVATED</color>");
+            }
+        }
+        else if (!isActive && _wasActive)
+        {
+            _menu.SetActive(false);
+            if (_showDebugLogs)
+            {
+                Debug.Log("<color=red>Wrist menu DEACTIVATED</color>");
+            }
+        }
+
+        _wasActive = isActive;
+    }
+}
